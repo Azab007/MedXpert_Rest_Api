79c24167289e35c8d76cc4a9b6df42a32d9931e6
Azab007 <57720086+Azab007@users.noreply.github.com>
2022-03-01 19:58:26 +0200

    update

--- controllers/drug.js
+++ controllers/drug.js
@@ -51,51 +51,35 @@ const deleteDrug =async (req, res) => {
 }
 
 
-const addInteraction = async(req, res) => {
+const addToList = async(req, res) => {
     
         const drug_id = req.query.id;
-
-        const drug = await Drug.findByIdAndUpdate(drug_id, {$push: {interactions: req.body}} , { runValidators: true, new: true });
-        if(!drug) {
-           throw Err("no drug matches this id", 404);
+        const {interactions, restrictions} = req.body
+        const newData = await Drug.findByIdAndUpdate(drug_id, {$addToSet: {interactions, restrictions}} , { runValidators: true, new: true });
+        if(!newData) {
+           throw Err("no matches for this id", 404);
         }
         
-        res.status(200).json({drug, msg:"the Drug interaction is added succesfully"});
+        res.status(200).json({newData, msg:"the data is added succesfully"});
 
 }
 
 
-const deleteInteraction = async (req, res) => {
+const deleteFromList = async (req, res) => {
   
-        const drug_id = req.query.id;
-        const drug = await Drug.findByIdAndUpdate(drug_id, {$pull: {interactions: {drug_id: mongoose.Types.ObjectId(req.body.drug_id)}}}, { runValidators: true, new: true });
-        if(!drug) {
-           throw Err("no drug matches this id", 404);
+        const id = req.query.id;
+        const drug_id = req.body.interactions ? req.body.interactions.drug_id : null ;
+        const condition_name = req.body.restrictions ? req.body.restrictions.condition_name : null;
+
+        const newData = await Drug.findByIdAndUpdate(id, {$pull: {interactions: {drug_id}, restrictions: {condition_name} }, }, { runValidators: true, new: true });
+        if(!newData) {
+           throw Err("no matches for this id", 404);
         }
-        res.status(200).json({msg: "the Drug interaction is deleted succesfully"});
+        res.status(200).json({newData, msg: "the data is deleted succesfully"});
     }
 
-const addRestrictions = async(req, res) => {
-        const drug_id = req.query.id;
-        const drug = await Drug.findByIdAndUpdate(drug_id, {$push: {restrictions: req.body}}, { runValidators: true, new: true });
-        if(!drug) {
-           throw Err("no drug matches this id", 404);
-        }
-        res.status(200).json("the Drug restriction is added succesfully");
-   
-}
 
-
-const deleteRestrictions = async (req, res) => {
-
-        const drug_id = req.query.id;
-        const drug = await Drug.findByIdAndUpdate(drug_id, {$pull: {restrictions: {condition_name: req.body.condition_name}}}, { runValidators: true, new: true });
-        if(!drug) {
-           throw Err("no drug matches this id", 404);
-        }
-        res.status(200).json("the Drug restriction is deleted succesfully");
-}
 
 module.exports = {
-    createDrug, getDrug, getAllDrugs, updateDrug, deleteDrug, addInteraction, deleteInteraction, addRestrictions, deleteRestrictions
+    createDrug, getDrug, getAllDrugs, updateDrug, deleteDrug, addToList, deleteFromList
 };
--- routes/drug.js
+++ routes/drug.js
@@ -1,14 +1,12 @@
 const router = require('express').Router();
-const {createDrug, getDrug, getAllDrugs, updateDrug, deleteDrug, addInteraction, deleteInteraction, addRestrictions, deleteRestrictions} = require('../controllers/drug.js');
+const {createDrug, getDrug, getAllDrugs, updateDrug, deleteDrug, addToList, deleteFromList} = require('../controllers/drug.js');
 
 router.route('/createDrug').post(createDrug);
 router.route('/getDrug').get(getDrug);
 router.route('/getAllDrugs').get(getAllDrugs);
 router.route('/updateDrug').patch(updateDrug);
-router.route('/addInteraction').patch(addInteraction);
-router.route('/deleteInteraction').patch(deleteInteraction);
-router.route('/addRestriction').patch(addRestrictions);
-router.route('/deleteRestriction').patch(deleteRestrictions);
+router.route('/addToList').patch(addToList);
+router.route('/deleteFromList').patch(deleteFromList);
 router.route('/deleteDrug').delete(deleteDrug);
 
 
