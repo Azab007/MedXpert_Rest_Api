f7aec24274e8c251be07da5d45a67ce8ff25043b
Azab007 <57720086+Azab007@users.noreply.github.com>
2022-03-02 18:03:39 +0200

    Update Patient.js

--- models/Patient.js
+++ models/Patient.js
@@ -5,11 +5,12 @@ const PatientSchema = new mongoose.Schema({
     email: {
         type: String,
         required: true,
-        unique: true,
+
         match: [
             /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/,
             'Please provide a valid email',
-        ]
+        ],
+        unique: true
     },
     username: {
         type: String,
