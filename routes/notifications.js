095c6be3d1766c7c33dd60ae980b9d15f85bae4f
Azab007 <57720086+Azab007@users.noreply.github.com>
2022-06-21 23:15:58 +0200

    Update notifications.js

--- routes/notifications.js
+++ routes/notifications.js
@@ -2,7 +2,7 @@ const router = require('express').Router();
 const { createNotification, getNotification, deleteNotification } = require('../controllers/notification.js');
 const { authenticateUser, isPatient } = require('../middleware/authentication');
 router.route('/createNotification').post(authenticateUser, isPatient, createNotification);
-router.route('/getNotfications').get(authenticateUser, isPatient, getNotification);
+router.route('/getNotifications').get(authenticateUser, isPatient, getNotification);
 router.route('/deleteNotification').delete(authenticateUser, isPatient, deleteNotification);
 
 
