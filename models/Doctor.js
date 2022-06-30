02a963a0baf116a60a7de53d332f6d6a42f46c5f
Azab007 <57720086+Azab007@users.noreply.github.com>
2022-07-01 01:14:20 +0200

    update

--- models/Doctor.js
+++ models/Doctor.js
@@ -18,9 +18,7 @@ const DoctorSchema = new mongoose.Schema({
     },
     password: {
         type: String,
-        required: true,
-        match: [/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/, "Minimum eight characters, at least one uppercase letter,\
-        one lowercase letter, one number and one special character"]
+        required: true
     },
 
     birthDate: {
--- models/Patient.js
+++ models/Patient.js
@@ -20,8 +20,6 @@ const PatientSchema = new mongoose.Schema({
     password: {
         type: String,
         required: true,
-        match: [/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/, "password should be Minimum eight characters, at least one uppercase letter,\
-        one lowercase letter, one number and one special character"],
     },
     birthDate: {
         type: Date,
